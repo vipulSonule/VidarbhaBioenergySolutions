46bc291df4ac48e429135f385a0227bea3865c71
vipul Sonule <vipulsonule71@gmail.com>
2025-05-10 12:24:37 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "https://vidarbhabioenergysolutions.com/"], // update this for frontend deployment
+  origin: [ "https://vidarbhabioenergysolutions.com/"], // update this for frontend deployment
   credentials: true,
 }));
 app.use(bodyParser.json());
